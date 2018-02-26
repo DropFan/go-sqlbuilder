a42ccf0cf1fd69337c147e640aee9d22e9025709
DropFan <DropFan@Gmail.com>
2018-02-26 13:46:27 +0800

    add *Query.String() method

--- query.go
+++ query.go
@@ -1,5 +1,10 @@
 package builder
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Query ...
 type Query struct {
 	Query string
@@ -12,4 +17,21 @@ func NewQuery(q string, args ...interface{}) *Query {
 		Query: q,
 		Args:  args,
 	}
+}
+
+func (q *Query) String() (str string) {
+	str = strings.Replace(q.Query, " = ?", " = '%v'", -1)
+	str = strings.Replace(str, " != ?", " != '%v'", -1)
+	str = strings.Replace(str, " <> ?", " <> '%v'", -1)
+	str = strings.Replace(str, " > ?", " > '%v'", -1)
+	str = strings.Replace(str, " >= ?", " >= '%v'", -1)
+	str = strings.Replace(str, " < ?", " < '%v'", -1)
+	str = strings.Replace(str, " <= ?", " <= '%v'", -1)
+	str = strings.Replace(str, " (?, ", " ('%v', ", -1)
+	str = strings.Replace(str, " (?)", " ('%v')", -1)
+	// str = strings.Replace(str, ", ?)", ", '%v')", -1)
+	str = strings.Replace(str, ", ?", ", '%v'", -1)
+	// str = strings.Replace(str, ", ? ", ", '%v' ", -1)
+	str = fmt.Sprintf(str, q.Args...)
+	return
 }
--- query_test.go
+++ query_test.go
@@ -37,3 +37,54 @@ func TestNewQuery(t *testing.T) {
 		})
 	}
 }
+
+func TestQuery_String(t *testing.T) {
+	tests := []struct {
+		name string
+		q    *Query
+		sql  string
+		args []interface{}
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test 1",
+			sql:  "SELECT * FROM `tablename` WHERE `k` = ?",
+			args: []interface{}{12135183350725},
+			want: "SELECT * FROM `tablename` WHERE `k` = '12135183350725'",
+		},
+		{
+			name: "test 2",
+			sql:  "SELECT * FROM `tablename` WHERE `k` = ? AND `k2` = ?",
+			args: []interface{}{12135183350725, "testv"},
+			want: "SELECT * FROM `tablename` WHERE `k` = '12135183350725' AND `k2` = 'testv'",
+		},
+		{
+			name: "test 3",
+			sql:  "UPDATE `tablename` SET `f` = ? WHERE `k` = ? AND `k2` = ?",
+			args: []interface{}{"fff", 12135183350725, "testv"},
+			want: "UPDATE `tablename` SET `f` = 'fff' WHERE `k` = '12135183350725' AND `k2` = 'testv'",
+		},
+		{
+			name: "test 4",
+			sql:  "UPDATE `tablename` SET `f` = ?, `g` = ? WHERE `k` = ? AND `k2` = ?",
+			args: []interface{}{"fff", "ggg", 12135183350725, "testv"},
+			want: "UPDATE `tablename` SET `f` = 'fff', `g` = 'ggg' WHERE `k` = '12135183350725' AND `k2` = 'testv'",
+		},
+		{
+			name: "test 5",
+			sql:  "INSERT INTO `tablename` VALUES (?, ?, ?, ?)",
+			args: []interface{}{"fff", "ggg", 12135183350725, "testv"},
+			want: "INSERT INTO `tablename` VALUES ('fff', 'ggg', '12135183350725', 'testv')",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.q = NewQuery(tt.sql, tt.args...)
+
+			if got := tt.q.String(); got != tt.want {
+				t.Errorf("Query.String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
